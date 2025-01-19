a08219bc25d500ebba279b1f99243699adc3bf2e
umutplvn <umutpehlivan2078@gmail.com>
2025-01-19 23:46:53 +0000

    up

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,51 @@
+"use strict"
+
+/* -------------------------------------------------------
+    NODEJS EXPRESS | Erin's Restaurant
+------------------------------------------------------- */
+
+const express = require('express')
+const mongoose=require("mongoose")
+
+const adminSchema = new mongoose.Schema({
+
+    username:{
+        type:String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    },
+
+    first_name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    last_name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+
+
+}, {collection:"admins", timeStamps:true})
+
+module.exports =mongoose.model("Admin", adminSchema)
