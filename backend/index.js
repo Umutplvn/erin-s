6737232074e26632139ed823281cbe0be7c2d92a
Umut <120283584+Umutplvn@users.noreply.github.com>
2025-02-20 16:52:13 +0000

    Up

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const PORT=process.env.PORT
 const HOST=process.env.HOST
 const {deletePastReservations} = require('./src/cron/jobs')
 /*--------------------------------------*/
+
 app.use(express.json())
 app.use(require('cors')())
 deletePastReservations()
